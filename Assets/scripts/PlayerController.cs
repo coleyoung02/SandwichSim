e39422799657009c3373cbd4335f023230392101
coleyoung02 <coleyoung741@gmail.com>
2024-02-02 15:15:30 -0800

    controller cleanup

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Hands hands;
-    [SerializeField] Camera cam;
-    [SerializeField] Rigidbody rb;
+    [SerializeField] private Hands hands;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 6;
     private bool usingHands;
     private float xRotation;
     // Start is called before the first frame update
@@ -21,25 +22,8 @@ public class PlayerController : MonoBehaviour
     {
         if (!usingHands)
         {
-            transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse X"));
-            Vector3 camRot = cam.gameObject.transform.rotation.eulerAngles;
-            float unclamped = xRotation - Input.GetAxis("Mouse Y");
-            float newX = Mathf.Clamp(unclamped, -90f, 90f);
-            xRotation = newX;
-            if (Mathf.Abs(unclamped - newX) > 1f )
-            {
-                Debug.Log(unclamped);
-            }
-            cam.transform.localRotation = Quaternion.Euler(newX, 0, 0);
-            //should do ground check
-            if (Input.GetKey(KeyCode.W))
-            {
-                rb.velocity = new Vector3((transform.forward * 6).x, rb.velocity.y, (transform.forward * 6).z);
-            }
-            else if (rb.velocity.magnitude > .02f)
-            {
-                rb.velocity = new Vector3(0, rb.velocity.y, 0);
-            }
+            rotate();
+            move();
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -47,4 +31,32 @@ public class PlayerController : MonoBehaviour
             usingHands = !usingHands;
         }
     }
+
+    private void rotate()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse X"));
+        Vector3 camRot = cam.gameObject.transform.rotation.eulerAngles;
+        float unclamped = xRotation - Input.GetAxis("Mouse Y");
+        float newX = Mathf.Clamp(unclamped, -90f, 90f);
+        xRotation = newX;
+        if (Mathf.Abs(unclamped - newX) > 1f)
+        {
+            Debug.Log(unclamped);
+        }
+        cam.transform.localRotation = Quaternion.Euler(newX, 0, 0);
+    }
+
+    private void move()
+    {
+        //either switch this all to force mode, or leave it setting the velocity and do that for all wasd
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector3((transform.forward * moveSpeed).x, rb.velocity.y, (transform.forward * moveSpeed).z);
+        }
+        else if (rb.velocity.magnitude > .02f)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
 }
