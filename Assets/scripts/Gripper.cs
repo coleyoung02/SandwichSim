f4316d99bac13cb4fed17c5695bc28ad0b5045d8
coleyoung02 <coleyoung741@gmail.com>
2024-02-02 18:45:16 -0800

    done?

--- Assets/scripts/Gripper.cs
+++ Assets/scripts/Gripper.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Gripper : MonoBehaviour
 {
-    [SerializeField] GameObject open;
-    [SerializeField] GameObject closed;
+    [SerializeField] private GameObject open;
+    [SerializeField] private GameObject closed;
+    [SerializeField] private List<GameObject> nearHand;
+    [SerializeField] private List<GameObject> inHand;
     private bool isOpen;
 
     private void Start()
@@ -13,10 +15,80 @@ public class Gripper : MonoBehaviour
         isOpen = true;
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            
+        }
+    }
+
     public void Toggle()
     {
+        if (isOpen)
+        {
+            tryGrab();
+        }
+        else
+        {
+            tryRelease();
+        }
         isOpen = !isOpen;
         open.SetActive(isOpen);
         closed.SetActive(!isOpen);
+    }
+
+    public bool tryGrab()
+    {
+        bool anything = false;
+        foreach (GameObject g in nearHand)
+        {
+            grab(g);
+            anything = true;
+        }
+        return anything;
+    }
+
+    public bool tryRelease()
+    {
+        bool anything = false;
+        foreach (GameObject g in inHand)
+        {
+            release(g);
+            anything = true;
+        }
+        inHand = new List<GameObject>();
+        return anything;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "frobbable")
+        {
+            nearHand.Add(other.gameObject);
+            Debug.Log("can grab " + other.gameObject.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (nearHand.Contains(other.gameObject))
+        {
+            nearHand.Remove(other.gameObject);
+            Debug.Log("canT grab " + other.gameObject.name);
+        }
+    }
+
+    private void grab(GameObject g)
+    {
+        g.transform.parent = gameObject.transform;
+        g.GetComponent<Rigidbody>().useGravity = false;
+        inHand.Add(g);
+    }
+
+    private void release(GameObject g)
+    {
+        g.transform.parent = null;
+        g.GetComponent<Rigidbody>().useGravity = true;
     }
 }
