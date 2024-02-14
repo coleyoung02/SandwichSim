7546df3f99e534ff1449c9684b52c5f658d57e61
coleyoung02 <coleyoung741@gmail.com>
2024-02-13 19:36:29 -0800

    string fun

--- Assets/scripts/GroceryUI.cs
+++ Assets/scripts/GroceryUI.cs
@@ -5,6 +5,30 @@ using UnityEngine;
 
 public class GroceryUI : MonoBehaviour
 {
+    public static string groceryItemToString(GroceryItem item)
+    {
+        switch (item) {
+            case GroceryItem.Bread: return "bread";
+            case GroceryItem.Tomato: return "tomato";
+            case GroceryItem.Lettuce: return "lettuce";
+            case GroceryItem.Pickle: return "pickle";
+            default: return "oh no";
+        }
+    }
+
+    public static GroceryItem StringToGroceryItem(string s)
+    {
+        switch (s)
+        {
+            case "bread": return GroceryItem.Bread;
+            case "tomato": return GroceryItem.Tomato;
+            case "lettuce": return GroceryItem.Lettuce;
+            case "pickle": return GroceryItem.Pickle;
+                //might break C#
+            default: return (GroceryItem)(-1);
+        }
+    }
+
     [SerializeField] private GameObject groceryListUI;
     [SerializeField] private GroceryList groceryList;
     [SerializeField] private GameObject groceryListRow;
@@ -23,11 +47,22 @@ public class GroceryUI : MonoBehaviour
 
     public void MarkItemComplete(int index)
     {
-
+        groceryListRows[index].text = START_STRIKETHROUGH_TAG +
+            stringAtIndex(index) +
+            END_STRIKETHROUGH_TAG;
+        ;
     }
 
     public void MarkItemNeeded(int index)
     {
+        groceryListRows[index].text = START_STRIKETHROUGH_TAG +
+            stringAtIndex(index) +
+            END_STRIKETHROUGH_TAG;
+        ;
+    }
 
+    private string stringAtIndex(int index)
+    {
+        return groceryItemToString(groceryList.GetItemsNeeded()[index]);
     }
 }
