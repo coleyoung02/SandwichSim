f07d8df0b9701879695b23bf1545e774ddfbf8d6
Bihan Dasgupta <bihand2005@gmail.com>
2024-03-01 04:14:25 -0800

    Changes made to StartGame() so start button works

--- Assets/scripts/MenuManager.cs
+++ Assets/scripts/MenuManager.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private string firstLevelName = "Level_1";
+    //[SerializeField] private string firstLevelName = "Level_1";
 
     public void StartGame()
     {
-        GameInstanceManager.Instance.UpdateGameState(GameState.Gameplay);
-        SceneManager.LoadScene(firstLevelName);
+        //GameInstanceManager.Instance.UpdateGameState(GameState.Gameplay);
+        //SceneManager.LoadScene(firstLevelName);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ResumeGame()
