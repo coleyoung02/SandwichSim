53d07e778c2e5b5f688d39fbbfefd900d00a550f
coleyoung02 <coleyoung741@gmail.com>
2024-03-06 15:05:21 -0800

    options ui update

--- Assets/scripts/UI/CustomButtonHover.cs
+++ Assets/scripts/UI/CustomButtonHover.cs
@@ -7,12 +7,41 @@ using UnityEngine.EventSystems;
 public class CustomButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private TextMeshProUGUI tm;
+    [SerializeField] private bool toggle;
     private string baseText;
+    [SerializeField] private bool toggleActiveDefault;
+    [SerializeField] private string playerPrefsIntName;
+    private bool toggleActive;
 
     void Start()
     {
         baseText = tm.text;
+        if (toggle)
+        {
+            int pref = PlayerPrefs.GetInt(playerPrefsIntName, -1);
+            if (pref >= 0)
+            {
+                if (pref == 0)
+                {
+                    toggleActive = false;
+                }
+                else
+                {
+                    toggleActive = true;
+                }
+            }
+            else if (!toggleActiveDefault)
+            {
+                toggleActive = false;
+            }
+            else
+            {
+                toggleActive = true;
+            }
+            tm.text = GetText();
+        }
     }
+
     private void OnEnable()
     {
         if (baseText != null)
@@ -23,11 +52,27 @@ public class CustomButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        tm.text = baseText;
+        tm.text = GetText();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        tm.text = "[" + baseText + "]";
+        tm.text = "[" + GetText() + "]";
+    }
+
+    private string GetText()
+    {
+        if (!toggle)
+        {
+            return baseText;
+        }
+        if (toggleActive)
+        {
+            return baseText + " (X)";
+        }
+        else
+        {
+            return baseText + " ( )";
+        }
     }
 }
